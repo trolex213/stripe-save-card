34b3c52569767844ee62855ce9a4ed04df936b50
Troy Zhang <troyz0213@gmail.com>
2025-05-01 02:18:59 -0400

    update

--- app.py
+++ app.py
@@ -9,14 +9,15 @@ def home():
 @app.route('/api/save-user', methods=['POST'])
 def save_user():
     data = request.get_json()
+    name = data.get("name")
     email = data.get("email")
-    phone = data.get("phone")
+    org = data.get("org")
 
-    # Log submitted info
     with open("signups.txt", "a") as f:
-        f.write(f"Email: {email}, Phone: {phone}\n")
+        f.write(f"Name: {name}, Email: {email}, Org: {org}\n")
 
     return jsonify({"status": "success"}), 200
+
 
 @app.route('/api/track-click', methods=['POST'])
 def track_click():
